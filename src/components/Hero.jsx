c96263de435359ddf0b71462666d8ccf78bd5f59
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-06-16 11:19:02 -0400

    added to hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import heroImage from "../assets/head-pic2.jpg"
 
 export function Hero() {
     return (
         <section className="hero-section section-container">
-            <img src="" alt="hero pic"/>
-            <h1>I'm Brittni</h1>
-            <p>Descriptions</p>
+            <img src={heroImage} alt="hero pic"/>
+            <div>
+                <p>Welcome, my name is</p>
+                <h1>Brittni Rohrer.</h1>
+                <h2>Front End Web Developer</h2>
+                <p>I build accessible and responsive websites.</p>
+            </div>
         </section>
     )
 }
