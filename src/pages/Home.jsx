af548b158015ffc29565e93b7cb5087372db857d
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-06-16 10:46:16 -0400

    added resume

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,19 @@ import { Hero } from "../components/Hero"
 //Style Sheet
 import "../styles/Home.css"
 
+import resume from "../assets/Brittni-Rohrer-Resume.pdf"
+
 export default function Home() {
     return (
         <div className="home-page">
             <Hero />
+            <div className="action-section">
+                <a 
+                    href={resume}
+                    target="blank">
+                    View Resume
+                </a>
+            </div>
         </div>
     )
 }
