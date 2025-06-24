5ab7f6798bb5e1853acb78f089fe312bebec6ee4
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-06-23 23:30:12 -0400

    adding hero img and design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,12 @@ import "../styles/hero.css"
 export function Hero() {
     return (
         <section className="hero-section section-container">
-            <p className="hero-text">Welcome, my name is</p>
-            <h1 className="hero-name">Brittni Rohrer.</h1>
-            <h1 className="hero-name color">Front End Web Developer</h1>
-            <p className="hero-text">I build accessible and responsive websites.</p>
+            <div>
+                <p className="hero-text">Welcome, my name is</p>
+                <h1 className="hero-name">Brittni Rohrer.</h1>
+                <h1 className="hero-name color">Front End Web Developer</h1>
+                <p className="hero-text">I build accessible and responsive websites.</p>
+            </div>
         </section>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,18 +8,24 @@ import { Hero } from "../components/Hero"
 import "../styles/Home.css"
 
 import resume from "../assets/Brittni-Rohrer-Resume.pdf"
+import heroImg from "../assets/hero-pic.png"
 
 export default function Home() {
     return (
         <div className="home-page">
-            <Hero />
-            <section className="action-section">
-                <a 
-                    href={resume}
-                    target="blank">
-                    View Resume
-                </a>
-            </section>
+            <div>
+                <Hero />
+                <section className="action-section">
+                    <a
+                        href={resume}
+                        target="blank">
+                        View Resume
+                    </a>
+                </section>
+            </div>
+            <div className="image-shape">
+                <img className="hero-img" src={heroImg} />
+            </div>
         </div>
     )
 }
