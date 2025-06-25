74177a9ec3717b08de74ca2acf10a09483550869
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-06-25 14:03:20 -0400

    changed responsive design home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ export function Hero() {
     return (
         <section className="hero-section section-container">
             <div>
-                <p className="hero-text">Welcome, my name is</p>
+                <p className="hero-text accent-color">Welcome, my name is</p>
                 <h1 className="hero-name">Brittni Rohrer.</h1>
-                <h1 className="hero-name color">Front End Web Developer</h1>
+                <h1 className="hero-name accent-color">Front-End Web Developer</h1>
                 <p className="hero-text">I build accessible and responsive websites.</p>
             </div>
         </section>
