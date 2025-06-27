872a16630387ea3c6129964eda7f9be21c0490ff
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-06-27 00:05:44 -0400

    modified project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -11,8 +11,8 @@ export default function ProjectCard({projects}) {
             {projects.map((project) => {
                 return (
                     <div className="project-card">
-                        <h3 className="project-name" key={project.id}>{project.title}</h3>
                         <img className="project-img" src={project.image} alt="image description"/>
+                        <h3 className="project-name" key={project.id}>{project.title}</h3>
                         <p className="project-description">{project.description}</p>
                         <div className="project-tool-container">
                             {project.tools.map((tool) => {
@@ -22,11 +22,11 @@ export default function ProjectCard({projects}) {
                             })}
                         </div>
                         <div className="project-links-container">
-                            <a href={project.live} aria-label="View live website">
-                                <i class="fa-solid fa-globe project-link"></i>
+                            <a href={project.live} className="project-link">
+                                <i class="fa-solid fa-globe"></i>
                             </a>
-                            <a href={project.code} aria-label="View code on github">
-                                <i class="fa-brands fa-github project-link"></i>
+                            <a href={project.code} className="project-link">
+                                <i class="fa-solid fa-code"></i>
                             </a>
                         </div>
                     </div>
