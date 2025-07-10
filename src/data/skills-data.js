402a708fcd9097a20f7e0ecbcdf680977d74f2ef
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-07-09 22:33:07 -0400

    skills section style

--- src/data/skills-data.js
+++ src/data/skills-data.js
@@ -1,39 +1,38 @@
 export const skills = [
 {
+    name: "React",
+    logo: "fa-brands fa-react skill-logo"
+},
+{
+    name: "Vue",
+    logo: "fa-brands fa-vuejs skill-logo"
+},
+{
     name: "Git",
-    logo: "fa-brands fa-square-git skill__logo"
+    logo: "fa-brands fa-square-git skill-logo"
 },
 {
     name: "HTML",
-    logo: "fa-brands fa-html5 skill__logo"
+    logo: "fa-brands fa-html5 skill-logo"
 },
 {
     name: "JavaScript",
-    logo: "fa-brands fa-js skill__logo"
+    logo: "fa-brands fa-js skill-logo"
 },
 {
     name: "CSS",
-    logo: "fa-brands fa-css3-alt skill__logo"
+    logo: "fa-brands fa-css3-alt skill-logo"
 },
 {
     name: "SCSS",
-    logo: "fa-brands fa-sass skill__logo"
+    logo: "fa-brands fa-sass skill-logo"
 },
 {
     name: "Bootstrap",
-    logo: "fa-brands fa-bootstrap skill__logo"
-},
-{
-    name: "Vue JS",
-    logo: "fa-brands fa-vuejs skill__logo"
+    logo: "fa-brands fa-bootstrap skill-logo"
 },
 {
     name: "Terminal",
-    logo: "fa-solid fa-terminal skill__logo"
+    logo: "fa-solid fa-terminal skill-logo"
 },
-{
-    name: "Nuxt",
-    logo: "nuxt-logo skill__logo"
-},
-
 ]
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ export default function About() {
             <h1 className="page-header">About Me</h1>
 
             <div>
-                <section className="section-container bg-em">
+                <section className="profile-section section-container">
                     <h2 className="section-header">Hello, I'm Brittni</h2>
                     <div className="flex-container">
                        <p>{aboutText}</p>
@@ -50,14 +50,15 @@ export default function About() {
 
             {/* Skills Section */}
             <div>
-                <section className="skills-section section-container bg-cr">
+                <section className="skills-section section-container">
                         <h2 className="section-header">My Skills</h2>
                         {/* map over skills data to list each skill */}
                         <div className="skill-card-container">
                             {skills.map((skill) => {
                                 return (
-                                    <div className="skill-card">
-                                        <h3 className="skill-name" key={skill.name}>{skill.name}</h3>
+                                    <div className="skill-card" key={skill.name}>
+                                        <i className={skill.logo}></i>
+                                        <h3 className="skill-name">{skill.name}</h3>
                                     </div>
                                 )
                             })}
