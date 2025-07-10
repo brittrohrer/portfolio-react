eddf46f5ea4d3f253917d8ff4dceaac90cb6dbb4
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-07-09 23:20:06 -0400

    created footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react' 
+import resume from '../assets/Brittni-Rohrer-Resume.pdf'
+import "../styles/footer.css"
 
 export function Footer() {
     return (
-        <>
-            <h2>Footer</h2>
-        </>
+        <div className='footer'>
+            <div className='footer-content'>
+                <p>
+                    <i className="fa-regular fa-envelope footer-icon"></i>
+                    brittnirohrer@gmail.com
+                </p>
+                <div className='vr'></div>
+                <a
+                    className='footer-resume-link'
+                    href={resume}
+                    target="blank">
+                    View Resume
+                </a>
+            </div>
+            <div className='footer-social-links'>
+                <a
+                    href=""
+                    arget="blank">
+                        <i className="fa-brands fa-github footer-link"></i>
+                </a>
+                <a
+                    href=""
+                    target="blank">
+                        <i className="fa-brands fa-linkedin footer-link"></i>
+                </a>
+            </div>
+            <div className='copyright-footer'>
+                <p>Brittni Rohrer 2025</p>
+            </div>
+        </div>
     )
 }
