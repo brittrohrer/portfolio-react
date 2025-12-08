0b2ef6b80af1585d4f9eef2520ee0a36264c4cfe
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-12-07 22:06:43 -0500

    not sure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,6 @@ import resume from "../assets/Brittni-Rohrer-Resume.pdf"
 export default function Home() {
     return (
         <div className="home-page">
-            <div>
                 <Hero />
                 <section className="action-section">
                     <div className="resume-container">
@@ -22,26 +21,27 @@ export default function Home() {
                             View Resume
                         </a>
                     </div>
-                    <div className="social-links-container">
-                        <a
-                            href=""
-                            target="blank">
-                            <i className="fa-brands fa-github social-link"></i>
-                        </a>
-                        <a
-                            href=""
-                            target="blank">
-                            <i className="fa-brands fa-linkedin social-link"></i>
-                        </a>
-                    </div>
-                    <div>
-                        <p>
-                            <i className="fa-regular fa-envelope social-link"></i>
-                            brittnirohrer@gmail.com
-                        </p>
+                    <div className='connect-section'>
+                        <div className="social-links-container">
+                            <a
+                                href=""
+                                target="blank">
+                                <i className="fa-brands fa-github social-link"></i>
+                            </a>
+                            <a
+                                href=""
+                                target="blank">
+                                <i className="fa-brands fa-linkedin social-link"></i>
+                            </a>
+                        </div>
+                        <div>
+                            <p>
+                                <i className="fa-regular fa-envelope"></i>
+                                brittnirohrer@gmail.com
+                            </p>
+                        </div>
                     </div>
                 </section>
-            </div>
         </div>
     )
 }
